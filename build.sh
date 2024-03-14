74fe62f67607c1cd8cf31c631120b495bafa200a
daniel.tp <daniel.tp@cock.li>
2024-03-15 03:12:13 +0800

    ok actually fix the script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-rm -rf .phantomjs &&
-mkdir -p ../../.phantomjs/2.1.1/x86_64-linux/ &&
+rm -rf ~/.phantomjs &&
+mkdir -p ~/.phantomjs/2.1.1/x86_64-linux/ &&
 tar xf phantomjs.tar.bz2 &&
-mv phantomjs-2.1.1-linux-x86_64/* ../../.phantomjs/2.1.1/x86_64-linux/ &&
+mv phantomjs-2.1.1-linux-x86_64/* ~/.phantomjs/2.1.1/x86_64-linux/ &&
 rm -rf phantomjs-2.1.1-linux-x86_64 &&
 bundle install &&
 bundle exec rails db:migrate &&
