d1b222cd3cce57cd6e5fd9612812186609412a10
daniel.tp <daniel.tp@cock.li>
2024-03-15 03:20:27 +0800

    fixed it for reals this time

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ mv phantomjs-2.1.1-linux-x86_64/* ~/.phantomjs/2.1.1/x86_64-linux/ &&
 rm -rf phantomjs-2.1.1-linux-x86_64 &&
 bundle install &&
 bundle exec rails db:migrate &&
-bundle exec rails assets:precompile
+bundle exec rails assets:precompile &&
+bundle binstubs bundler --force &&
 echo done!
